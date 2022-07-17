730168f8f1f437274b49e2546d92ad39d323ce31
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2022-07-17 10:44:46 +0900

    add: 色メモを表示させるコンポーネントの基礎の部分を作成
    クラス名やファイル構造は要修正

---
+++ frontend/src/components/ColorMemoCard.tsx
@@ -0,0 +1,10 @@
+import { ColorMemoDataType } from "../types";
+const ColorMemoCard = () => {
+    return(
+        <div className="color-memo-card">
+            aaa
+        </div>
+    )
+}
+
+export default ColorMemoCard;
---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+const Footer = () => {
+    return (
+        <div className="footer">
+
+        </div>
+    )
+};
+export default Footer;
