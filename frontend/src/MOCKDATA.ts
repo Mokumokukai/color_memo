35d85b9eaae24f9b7dbca7a0f521df73b3e01ae5
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2022-07-24 19:20:38 +0900

    add: ColorPairのモックデータを追加

---
+++ frontend/src/MOCKDATA.ts
@@ -0,0 +1,41 @@
+import { ColorPair } from "./types";
+export const MOCK_COLORS:ColorPair[] = [
+    ({
+      id: 1,
+      colorPair: ["#292929FF","#f5f5f5ff"]
+    }),
+    ({
+        id: 2,
+        colorPair: ["#b8e729ff","#4415dbff"]
+    }),
+    ({
+        id:3,
+        colorPair: ["#add84fff","#5024b2ff"]
+    }),
+    ({
+        id:4,
+        colorPair:["#d2ac8bff","#2d4f6dff"]
+    }),
+    ({
+        id:5,
+        colorPair:["#00135dff", "#6c6857ff"]
+    }),
+    ({
+        id:6,
+        colorPair:["#ede5f1ff", "#11160eff"]
+    }),
+    ({
+        id:7,
+        colorPair:["#f5afd8ff", "#044f23ff"]
+    }),
+    ({
+        id:8,
+        colorPair:["#bfb554ff", "#3d47a3ff"]
+    }),
+    ({
+        id:9,
+        colorPair:["#bfb554ff", "#3d47a3ff"]
+    })
+
+
+  ];
