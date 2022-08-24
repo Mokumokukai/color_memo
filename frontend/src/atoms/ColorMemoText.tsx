3ea5da89e25bb88f3de549c0196f6b0ef8386b6a
rikutkb <36298176+rikutkb@users.noreply.github.com>
2022-08-24 20:43:03 +0900

    Revert "fix: tailwindのapplyが使用できるように設定"

--- frontend/src/atoms/ColorMemoText.tsx
+++ frontend/src/atoms/ColorMemoText.tsx
@@ -1,19 +1,27 @@
-import "./ColorMemoText.css";
-
 type Props = {
   color: string;
   typoCss: object;
   insideText?: string;
 };
+const textCss = {
+  fontSize: '10vw',
+  textAlign: 'center',
+  lineHeight: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexDirection: 'column',
+};
 
 function ColorMemoText({ color, typoCss, insideText }: Props) {
   const style: React.CSSProperties = {
     ...typoCss,
-    color
+    color,
+    ...textCss,
   } as React.CSSProperties;
 
   return (
-    <div style={style} className="text">
+    <div style={style}>
       <p>{insideText}</p>
     </div>
   );
